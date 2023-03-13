b3394d6e54902e0fd0b22ce320b4f8a997af7f20
Tom <454147@student.fontys.nl>
2023-03-13 15:26:26 +0100

    + postUser() + deleteUser() + getUser()
    there are multiple conflicts with userInterface and userModel. I will fix these before a push to main

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,21 @@
 import { IUserRepository } from "../interfaces/user.interface";
-import User from "../models/user.model";
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import User from './models/user';
 
+const app = express();
+
+mongoose
+  .connect('mongodb+srv://Backend:aTLOpFcGdeLuaRlG', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error(err));
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+const connectionString = "mongodb+srv://Backend:aTLOpFcGdeLuaRlG@<your-cluster-url>/test?retryWrites=true&w=majority";
+const client = new MongoClient(connectionString);
 
 export default class UserRepository implements IUserRepository {
     constructor() {}
@@ -13,5 +28,56 @@ public async postSignInCredentials(user: User): Promise<void> {
     
 }
 
+public async postUser(user: User): Promise<void> {
+  app.post('/users', async (req: Request, res: Response) => {
+    try {
+        const savedUser = await user.save();
+  
+      res.status(201).json(savedUser);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+}
+
+public async deleteUser(user: User): Promise<void> {
+  app.delete('/users/:id', async (req: Request, res: Response) => {
+    try {
+      const deletedUser = await User.findByIdAndDelete(req.params.id);
+  
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.status(200).json({ message: 'User deleted', deletedUser });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+}
+
+public async getUser(): Promise<User> {
+  app.get('/users', async (req: Request, res: Response) => {
+    try {
+      const users = await User.find();
+  
+      if (users.length === 0) {
+        return res.status(404).json({ message: 'No users found' });
+      }
+  
+      res.status(200).json(users);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+  
+}
+    
+public async updateUser(user: User): Promise<void> {
+    
+}
 
 }
