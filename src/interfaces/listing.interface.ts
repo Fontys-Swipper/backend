61bf9fdff665c096bdd25ba0f44ff351bab48828
Tom <454147@student.fontys.nl>
2023-04-17 11:27:10 +0200

    updated listing.repository.ts updated listing.interface.ts updated ormconfig.json

--- src/interfaces/listing.interface.ts
+++ src/interfaces/listing.interface.ts
@@ -4,21 +4,21 @@ import express, { Request, Response } from "express";
 export interface IListingController {
     postListing(req: Request, res: Response):Promise<void>;
     deleteListing(req: Request, res: Response): Promise<void>;
-    getListing(req: Request, res: Response): Promise<Listing>;
+    getListing(req: Request, res: Response): Promise<void>;
     updateListing(req: Request, res: Response): Promise<void>;
 }
 
 
 export interface IListingService {
-    postListing(listing: Listing): Promise<void>;
-    deleteListing(listing: Listing): Promise<void>;
-    getListing(): Promise<void>;
-    updateListing(listing: Listing): Promise<void>;
+    postListing(req: Request, res: Response):Promise<void>;
+    deleteListing(req: Request, res: Response): Promise<void>;
+    getListing(req: Request, res: Response): Promise<void>;
+    updateListing(req: Request, res: Response): Promise<void>;
 }
 
 export interface IListingRepository {
-    postListing(listing: Listing): Promise<void>;
-    deleteListing(listing: Listing): Promise<void>;
-    getListing(): Promise<Listing>;
-    updateListing(listing: Listing): Promise<void>;
+    postListing(req: Request, res: Response):Promise<void>;
+    deleteListing(req: Request, res: Response): Promise<void>;
+    getListing(req: Request, res: Response): Promise<void>;
+    updateListing(req: Request, res: Response): Promise<void>;
 }
--- src/repositories/listing.repository.ts
+++ src/repositories/listing.repository.ts
@@ -1,76 +1,77 @@
+import { IListingRepository } from "../interfaces/listing.interface";
 import { NextFunction, Request, Response } from 'express';
 import {getManager} from "typeorm";
 import {Listing} from "../models/listing.model";
-import { IUserRepository } from "../interfaces/user.interface";
-import { User } from '../models/user.model';
+import { ParamsDictionary } from "express-serve-static-core";
+import { ParsedQs } from "qs";
 
-export default class UserRepository implements IUserRepository {
-    constructor() {
+export default class ListingRepository implements IListingRepository {
+  constructor() {}
 
-    }
-
-    async postUser(request: Request, response: Response) {
-
-      const UserRepository = getManager().getRepository(User);
-      const newUser = UserRepository.create(request.body);
-  
-      await UserRepository.save(newUser);
-  
-      response.send(newUser);
-    }
-
+    async postListing(request: Request, response: Response) {
+        const listingRepository = getManager().getRepository(Listing);
+        const newPost = listingRepository.create(request.body);
     
+        await listingRepository.save(newPost);
+    
+        response.send(newPost);
+    }
 
-    async getUser(request: Request, response: Response) {
-
-      const UserRepository = getManager().getRepository(User);
-      const user = await UserRepository.findOneBy({
-        id: Number(request.params),
+    async deleteListing(request: Request, response: Response) {
+      const listingRepository = getManager().getRepository(Listing);
+      const { id } = request.params;
+      const listing = await listingRepository.findOneBy({
+          listing_id: Number(request.params.id),
       });
   
-      // if post was not found return 404 to the client
-      if (!user) {
+      
+      if (!listing) {
+        response.status(404);
+        response.end();
+        return;
+      }
+      
+      await listingRepository.remove(listing);
+      
+      response.send(listing);
+    }
+
+    async getListing(request: Request, response: Response) {
+
+        const listRepository = getManager().getRepository(Listing);
+        const listing = await listRepository.findOneBy({
+            listing_id: Number(request.params.id),
+        });
+    
+        // if post was not found return 404 to the client
+        if (!listing) {
+            response.status(404);
+            response.end();
+            return;
+        }
+        response.send(listing);
+    }
+
+    async updateListing(request: Request, response: Response) {
+        const listingRepository = getManager().getRepository(Listing);
+        const { id } = request.params;
+        const listing = await listingRepository.findOneBy({
+            listing_id: Number(request.params.id),
+        });
+    
+        
+        if (!listing) {
           response.status(404);
           response.end();
           return;
+        }
+        
+        listingRepository.merge(listing, request.body);
+        const updatedListing = await listingRepository.save(listing);
+        
+        response.send(updatedListing);
       }
-      response.send(user);
-  }
-
-  async updateUser(request: Request, response: Response) {
-    const UserRepository = getManager().getRepository(User);
-    const { id } = request.params;
-    const user = await UserRepository.findOneBy({
-      id: Number(request.params),
-    });
-    
-    if (!user) {
-      response.status(404);
-      response.end();
-      return;
-    }
-    
-    UserRepository.merge(user, request.body);
-    const updatedUser = await UserRepository.save(user);
-    
-    response.send(updatedUser);
-  }
-
-  async deleteUser(request: Request, response: Response) {
-    const UserRepository = getManager().getRepository(User);
-    const { id } = request.params;
-    const user = await UserRepository.findOneBy({
-      id: Number(request.params),
-    });
-    
-    if (!user) {
-      response.status(404);
-      response.end();
-      return;
-    }
-    
-    await UserRepository.remove(user);
-    
-    response.send(user);
-  }
+      
+      
 }
+
